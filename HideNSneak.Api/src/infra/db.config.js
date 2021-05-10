77a221f2cad6f12d7bed7f038f4793bf1305e274
mh-developer <mh7860@student.uni-lj.si>
2021-05-11 00:13:21 +0200

    feat: setup connection to mongo db with mongoose and prepare users and locations repositories

---
+++ HideNSneak.Api/src/infra/db.config.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+    const mongoDB = process.env.DB_CONNECTION_STRING;
+
+    mongoose.connect(mongoDB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    const db = mongoose.connection;
+
+    db.on("error", console.error.bind(console, "MongoDB connection error:"));
+};
---
+++ HideNSneak.Api/src/infra/repositories/users.repository.js
@@ -0,0 +1,36 @@
+const { UserModel } = require("../../domain/index");
+
+const getAll = async () => {
+    return await UserModel.find();
+};
+
+const get = async (id) => {
+    return await UserModel.findById(id);
+};
+
+const create = async (data) => {
+    const newUser = new UserModel(data);
+    return await newUser.save();
+};
+
+const update = async (id, data) => {
+    return await UserModel.findByIdAndUpdate(id, {
+        ...data,
+        updated: Date.now(),
+    });
+};
+
+const remove = async (id) => {
+    const user = await UserModel.findById(id);
+    await user.deleteOne();
+};
+
+const usersRepository = {
+    getAll,
+    get,
+    create,
+    update,
+    remove,
+};
+
+module.exports = usersRepository;
