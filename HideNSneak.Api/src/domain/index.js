0403164633c51fb68452688370b3807ab3c5bcd5
mh-developer <mh7860@student.uni-lj.si>
2021-05-11 00:07:10 +0200

    feat: prepare users and locations domain layer

---
+++ HideNSneak.Api/src/domain/index.js
@@ -0,0 +1,7 @@
+const UserModel = require("./users/users.model");
+const LocationModel = require("./locations/location.model");
+
+module.exports = {
+    UserModel,
+    LocationModel,
+};
---
+++ HideNSneak.Api/src/domain/users/users.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    id: { type: Schema.Types.ObjectId, required: true, auto: true },
+    name: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true },
+    created: { type: Date, default: Date.now },
+    updated: { type: Date, default: Date.now },
+});
+
+UserSchema.virtual("url").get(function () {
+    return "/users/" + this.id;
+});
+
+module.exports = mongoose.model("UserModel", UserSchema);
