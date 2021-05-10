6244ef5d409a0dfbd8a060ba8255987153a6fbac
mh-developer <mh7860@student.uni-lj.si>
2021-05-10 18:36:51 +0200

    feat: add locations and users tests

---
+++ HideNSneak.Api/test/unit/app/locations/locations.spec.js
@@ -0,0 +1,6 @@
+const { locationsService } = require("../../../../src/app/index");
+
+test("test getting all locations", async () => {
+    const data = await locationsService.getAll();
+    expect(data).toBeDefined();
+});
