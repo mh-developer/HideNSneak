84b6c1411dbb584b787e8c4ebf30eac3f5bdac06
mh-developer <mh7860@student.uni-lj.si>
2021-05-17 16:52:22 +0200

    refactor: change strategy of passing unitOfWork

--- HideNSneak.Api/src/app/auth/auth.service.js
+++ HideNSneak.Api/src/app/auth/auth.service.js
@@ -1,37 +1,11 @@
-const unitOfWork = require('../../infra/unit-of-work');
 const jwt = require('jsonwebtoken');
 
-const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET || 'devModeSecret';
-const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET || 'devModeRefresh';
+module.exports = (unitOfWork = require('../../infra/unit-of-work')) => {
+  const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET || 'devModeSecret';
+  const refreshTokenSecret =
+    process.env.REFRESH_TOKEN_SECRET || 'devModeRefresh';
 
-const login = async user => {
-  const data = { id: user.id, email: user.email };
-  const accessToken = jwt.sign({ user: data }, accessTokenSecret, {
-    expiresIn: '20m'
-  });
-  const refreshTokenJwt = jwt.sign({ id: user.id }, refreshTokenSecret);
-
-  return { access_token: accessToken, refresh_token: refreshTokenJwt };
-};
-
-const register = async data => {
-  return await unitOfWork.users.create(data);
-};
-
-const logout = async oldRefreshToken => {
-  // TODO: remove saved refresh token
-  return new Promise((resolve, reject) => {
-    resolve(true);
-  });
-};
-
-const refreshToken = (user, oldRefreshToken) => {
-  // TODO: refresh token should be saved and verified from db
-
-  jwt.verify(oldRefreshToken, refreshTokenSecret, (err, token) => {
-    if (err) {
-      throw new Error('Forbidden');
-    }
+  const login = async user => {
     const data = { id: user.id, email: user.email };
     const accessToken = jwt.sign({ user: data }, accessTokenSecret, {
       expiresIn: '20m'
@@ -39,14 +13,42 @@ const refreshToken = (user, oldRefreshToken) => {
     const refreshTokenJwt = jwt.sign({ id: user.id }, refreshTokenSecret);
 
     return { access_token: accessToken, refresh_token: refreshTokenJwt };
-  });
-};
+  };
 
-const authService = {
-  login,
-  register,
-  logout,
-  refreshToken
-};
+  const register = async data => {
+    return await unitOfWork.users.create(data);
+  };
 
-module.exports = authService;
+  const logout = async oldRefreshToken => {
+    // TODO: remove saved refresh token
+    return new Promise((resolve, reject) => {
+      resolve(true);
+    });
+  };
+
+  const refreshToken = (user, oldRefreshToken) => {
+    // TODO: refresh token should be saved and verified from db
+
+    jwt.verify(oldRefreshToken, refreshTokenSecret, (err, token) => {
+      if (err) {
+        throw new Error('Forbidden');
+      }
+      const data = { id: user.id, email: user.email };
+      const accessToken = jwt.sign({ user: data }, accessTokenSecret, {
+        expiresIn: '20m'
+      });
+      const refreshTokenJwt = jwt.sign({ id: user.id }, refreshTokenSecret);
+
+      return { access_token: accessToken, refresh_token: refreshTokenJwt };
+    });
+  };
+
+  const authService = {
+    login,
+    register,
+    logout,
+    refreshToken
+  };
+
+  return authService;
+};
--- HideNSneak.Api/src/app/index.js
+++ HideNSneak.Api/src/app/index.js
@@ -1,6 +1,10 @@
-const locationsService = require('./locations/locations.service');
-const usersService = require('./users/users.service');
-const authService = require('./auth/auth.service');
+const locations = require('./locations/locations.service');
+const users = require('./users/users.service');
+const auth = require('./auth/auth.service');
+
+const authService = auth();
+const usersService = users();
+const locationsService = locations();
 
 module.exports = {
   locationsService,
