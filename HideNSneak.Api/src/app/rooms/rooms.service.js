1f9c624907de268f1a5054f289a899df50c2bc3e
Marko Gluhak <marko.gluhak@student.um.si>
2021-05-18 14:03:18 +0000

    feat: added room joining and quitting functionality. Creator is automatically...

--- HideNSneak.Api/src/app/rooms/rooms.service.js
+++ HideNSneak.Api/src/app/rooms/rooms.service.js
@@ -12,11 +12,48 @@ module.exports = (unitOfWork = require('../../infra/unit-of-work')) => {
   };
 
   const create = async data => {
+    data.joinCode = (+new Date()).toString(36).slice(-4);
     return await unitOfWork.rooms.create(data);
   };
 
   const update = async (id, data) => {
     return await unitOfWork.rooms.update(id, data);
+  };
+
+  const join = async data => {
+    const room = await getByFilter(
+      { joinCode: data.joinCode },
+      function (err, obj) {
+        console.log(err, obj);
+      }
+    );
+
+    if (room.currentPlayers.indexOf(data.userId) === -1) {
+      room.currentPlayers.push(data.userId);
+    } else {
+      console.log('User already in the room');
+      return room;
+    }
+    room.save();
+    return room;
+  };
+
+  const quit = async data => {
+    const room = await getByFilter(
+      { joinCode: data.joinCode },
+      function (err, obj) {
+        console.log(err, obj);
+      }
+    );
+
+    if (room.currentPlayers.indexOf(data.userId) !== -1) {
+      room.currentPlayers.splice(room.currentPlayers.indexOf(data.userId), 1);
+    } else {
+      console.log('User is not in room');
+      return room;
+    }
+    room.save();
+    return room;
   };
 
   const remove = async id => {
@@ -29,7 +66,9 @@ module.exports = (unitOfWork = require('../../infra/unit-of-work')) => {
     getByFilter,
     create,
     update,
-    remove
+    remove,
+    join,
+    quit
   };
 
   return roomsService;
