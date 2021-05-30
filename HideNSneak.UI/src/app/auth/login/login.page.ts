71fdcb1ed95643eef4b7d5b0552f0025f3716bd6
mh-developer <mh7860@student.uni-lj.si>
2021-05-30 09:39:05 +0200

    feat: prepare login page

--- HideNSneak.UI/src/app/auth/login/login.page.ts
+++ HideNSneak.UI/src/app/auth/login/login.page.ts
@@ -1,63 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
-import { ModalController, AlertController } from '@ionic/angular';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { ToastController } from '@ionic/angular';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.page.html',
     styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
-    pepperoni: string;
-    email: string;
-    pass: string;
-    dataa: any;
+export class LoginPage implements OnInit, OnDestroy {
+    private unsubscribe$ = new Subject<void>();
+
+    public invalidLogin: boolean;
+    public loading: boolean = false;
+    public authForm: FormGroup;
 
     constructor(
         public router: Router,
-        public modalController: ModalController,
-        public alertController: AlertController
+        private authService: AuthService,
+        private fb: FormBuilder,
+        public toastController: ToastController
     ) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.authForm = this.fb.group({
+            email: ['', Validators.required],
+            password: ['', Validators.required],
+        });
+    }
 
-    signup() {
+    ngOnDestroy() {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
+    }
+
+    public signin() {
+        this.loading = true;
+        const credentials = this.authForm.value;
+
+        this.authService
+            .attemptAuth('login', credentials)
+            .pipe(takeUntil(this.unsubscribe$))
+            .subscribe(
+                (res) => {
+                    this.invalidLogin = false;
+                    this.router.navigateByUrl('/tabs');
+                },
+                (err) => {
+                    this.invalidLogin = true;
+                    this.loading = false;
+                    this.showError(err);
+                },
+                () => {
+                    this.loading = false;
+                }
+            );
+    }
+
+    public signup() {
         this.router.navigateByUrl('/signup');
     }
 
-    forget() {
+    public forget() {
         this.router.navigateByUrl('/forget');
     }
 
-    home() {
-        this.router.navigateByUrl('/tabs');
-        //   this.dataa= {EmailAddress:this.email,Password:this.pass,PlayerID :""};
-
-        //   this.http.post('https://webapi.now-open.co.uk/api/userlogin',this.dataa, this.httpOptions).subscribe( async (data:any) => {
-        //    console.log(data);
-        //   var mydata= data;
-        //   if(mydata.ID > 0){
-        //     this.http.get('https://webapi.now-open.co.uk/api/userprofile?uid='+mydata.ID,this.httpOptions).subscribe(async data => {
-        //    console.log(mydata.ImageURL);
-        //    console.log(mydata.Name);
-
-        //     const modal = await this.modalController.create({
-        //     component: TestPage,
-        //     componentProps: { user: data }
-        //     });
-        //     return await modal.present();
-
-        //   });
-        // }
-        // if(mydata.ID == 0){
-        //   let alert =   await this.alertController.create({
-        //     header: 'User Detail',
-        //     message: 'No user found',
-        //     buttons: ['Dismiss']
-        //   });
-        //   await alert.present();
-        // }
-        //  });
+    private async showError(err) {
+        const toast = await this.toastController.create({
+            message: 'Wrong email or password. Please try again.',
+            duration: 5000,
+            color: 'danger',
+        });
+        toast.present();
     }
 }
