fef3388da67ba2ccffff9efab7801a3a0fbcf681
mh-developer <mh7860@student.uni-lj.si>
2021-06-02 10:33:23 +0200

    wip: add support for getting geolocation

--- HideNSneak.UI/src/app/game/home/home.component.ts
+++ HideNSneak.UI/src/app/game/home/home.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PopoverController } from '@ionic/angular';
 import { DropdownPage } from '../../shared/dropdown/dropdown/dropdown.page';
 import { AuthService } from '../../auth/shared/auth.service';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import {
+    NativeGeocoder,
+    NativeGeocoderOptions,
+    NativeGeocoderResult,
+} from '@ionic-native/native-geocoder/ngx';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
     tab: string;
     tab2: string;
     title: string = 'HideNSeek project';
@@ -18,13 +24,76 @@ export class HomeComponent {
     zoom: number = 6;
     styles: any;
 
+    lat: any;
+    long: any;
+    address: any;
+    accuracy: any;
+
     constructor(
         public router: Router,
         public popoverController: PopoverController,
-        private authService: AuthService
+        private authService: AuthService,
+
+        private geolocation: Geolocation,
+        private nativeGeocoder: NativeGeocoder
     ) {
         this.tab = 'create';
         this.tab2 = 'all';
+    }
+
+    ngOnInit() {}
+
+    options = {
+        timeout: 12000,
+        maximumAge: 3600,
+        enableHighAccuracy: true,
+    };
+
+    geoInformation() {
+        this.geolocation
+            .getCurrentPosition()
+            .then((data) => {
+                this.lat = data.coords.latitude;
+                this.long = data.coords.longitude;
+                this.accuracy = data.coords.accuracy;
+
+                this.cordsToAddress(this.lat, this.long);
+            })
+            .catch((error) => {
+                alert(error);
+            });
+    }
+
+    // geocoder options
+    nativeGeocoderOptions: NativeGeocoderOptions = {
+        maxResults: 6,
+        useLocale: true,
+    };
+
+    // reverse geocode
+    cordsToAddress(lat, long) {
+        this.nativeGeocoder
+            .reverseGeocode(lat, long, this.nativeGeocoderOptions)
+            .then((response: NativeGeocoderResult[]) => {
+                this.address = this.createAddress(response[0]);
+            })
+            .catch((error: any) => {
+                alert(JSON.stringify(error));
+            });
+    }
+
+    // Create address
+    createAddress(addressObject) {
+        let object = [];
+        let address = '';
+        for (let key in addressObject) {
+            object.push(addressObject[key]);
+        }
+        object.reverse();
+        for (let val in object) {
+            if (object[val].length) address += object[val] + ', ';
+        }
+        return address.slice(0, -2);
     }
 
     async dropdown(event) {
