940c44c7e73616fb19436dc7c668abb1e13eac10
mh-developer <mh7860@student.uni-lj.si>
2021-06-06 11:02:57 +0200

    feat: prepare basic game logic

--- HideNSneak.UI/src/app/game/game.module.ts
+++ HideNSneak.UI/src/app/game/game.module.ts
@@ -6,9 +6,15 @@ import { SharedModule } from '../shared/shared.module';
 import { AgmCoreModule } from '@agm/core';
 import { environment } from './../../environments/environment';
 import { GameSettingsComponent } from './game-settings/game-settings.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
-    declarations: [HomeComponent, MapComponent, GameSettingsComponent],
+    declarations: [
+        HomeComponent,
+        MapComponent,
+        GameSettingsComponent,
+        GameComponent,
+    ],
     imports: [
         SharedModule,
         GameRoutingModule,
--- HideNSneak.UI/src/app/game/shared/models/game.model.ts
+++ HideNSneak.UI/src/app/game/shared/models/game.model.ts
@@ -3,3 +3,14 @@ export interface PlayerLocation {
     lng: number;
     lat: number;
 }
+
+export const colorOptions = [
+    { key: 'primary', value: '#1FBAD6' },
+    { key: 'secondary', value: '#3dc2ff' },
+    { key: 'tertiary', value: '#5260ff' },
+    { key: 'success', value: '#2dd36f' },
+    { key: 'warning', value: '#ffc409' },
+    { key: 'light', value: '#f4f5f8' },
+    { key: 'medium', value: '#92949c' },
+    { key: 'dark', value: '#222428' },
+];
