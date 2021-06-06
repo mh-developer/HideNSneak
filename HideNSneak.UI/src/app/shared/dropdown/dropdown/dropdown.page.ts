c8c38bec04c5dd3b072bc1bd0a82d4caf3872a58
mh-developer <mh7860@student.uni-lj.si>
2021-06-06 11:03:57 +0200

    refactor: remove unused dropdown

--- HideNSneak.UI/src/app/shared/dropdown/dropdown/dropdown.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-dropdown',
-    templateUrl: './dropdown.page.html',
-    styleUrls: ['./dropdown.page.scss'],
-})
-export class DropdownPage {}
